14c3cd55a420d154de3b9ea455ca2db5589a0b3b
hannes <hannes@octofox.de>
2025-10-21 11:44:08 +0200

    wip: Do not unmount stream on pause

--- android/src/main/java/video/api/reactnative/livestream/LiveStreamView.kt
+++ android/src/main/java/video/api/reactnative/livestream/LiveStreamView.kt
@@ -258,11 +258,25 @@ class LiveStreamView @JvmOverloads constructor(
   }
 
   override fun onHostPause() {
-    liveStream.stopStreaming()
+    // App or fragment lost focus. Keep streaming. Only stop preview to free the Surface.
     liveStream.stopPreview()
   }
 
   override fun onHostDestroy() {
     liveStream.release()
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+
+    if (permissionsManager.hasPermission(Manifest.permission.CAMERA)) {
+      liveStream.startPreview()
+    }
+  }
+
+  override fun onDetachedFromWindow() {
+    super.onDetachedFromWindow()
+    // Detach preview when the view is not in the window. Do not stop streaming.
+    liveStream.stopPreview()
   }
 }
--- ios/RNLiveStreamViewImpl.swift
+++ ios/RNLiveStreamViewImpl.swift
@@ -201,12 +201,17 @@ public class RNLiveStreamViewImpl: UIView {
     @objc public var onDisconnect: (_ dictionnary: [String: Any]) -> Void = { _ in }
 
     @objc public var onStartStreaming: (_ dictionnary: [String: Any]) -> Void = { _ in }
-    
-    @objc override public func removeFromSuperview() {
-        super.removeFromSuperview()
+
+    override public func didMoveToWindow() {
+      super.didMoveToWindow()
+      if window != nil {
+        // Re-attach preview when the view is back on-screen.
+        liveStream.startPreview()
+      } else {
         liveStream.stopPreview()
+      }
     }
-}
+
 
 extension RNLiveStreamViewImpl: ApiVideoLiveStreamDelegate {
     /// Called when the connection to the rtmp server is successful
