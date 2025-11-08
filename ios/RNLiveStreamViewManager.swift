35945c2b5aecfc30d6ccd9ccc6893c37e18a977a
H. C. Kruse <hannes@octofox.de>
2025-11-08 17:22:42 +0100

    fix: fix force unwrap

--- ios/RNLiveStreamViewManager.swift
+++ ios/RNLiveStreamViewManager.swift
@@ -1,38 +1,59 @@
 import ApiVideoLiveStream
 import CoreGraphics
 import Foundation
+import React
 
 @objc(RNLiveStreamViewManager)
 class RNLiveStreamViewManager: RCTViewManager {
-    override static func requiresMainQueueSetup() -> Bool {
-        return true
+  override static func requiresMainQueueSetup() -> Bool { true }
+
+  override func view() -> RNLiveStreamViewImpl { RNLiveStreamViewImpl() }
+
+  @objc(startStreaming:withRequestId:withStreamKey:withUrl:)
+  func startStreaming(_ reactTag: NSNumber, withRequestId requestId: NSNumber, streamKey: String, url: String?) {
+    guard let uiManager = bridge?.uiManager else {
+      NSLog("[RNLiveStreamViewManager] bridge/uiManager is nil")
+      return
     }
 
-    override func view() -> (RNLiveStreamViewImpl) {
-        return RNLiveStreamViewImpl()
+    uiManager.addUIBlock { uiManager, _ in
+      guard let view = uiManager?.view(forReactTag: reactTag) as? RNLiveStreamViewImpl else {
+        NSLog("[RNLiveStreamViewManager] view for tag \(reactTag) not found or wrong type")
+        return
+      }
+      view.startStreaming(requestId: requestId.intValue, streamKey: streamKey, url: url)
+    }
+  }
+
+  @objc(stopStreaming:)
+  func stopStreaming(_ reactTag: NSNumber) {
+    guard let uiManager = bridge?.uiManager else {
+      NSLog("[RNLiveStreamViewManager] bridge/uiManager is nil")
+      return
     }
 
-    @objc(startStreaming:withRequestId:withStreamKey:withUrl:)
-    func startStreaming(_ reactTag: NSNumber, withRequestId requestId: NSNumber, streamKey: String, url: String?) {
-        bridge!.uiManager.addUIBlock { (_: RCTUIManager?, viewRegistry: [NSNumber: UIView]?) in
-            let view: RNLiveStreamViewImpl = (viewRegistry![reactTag] as? RNLiveStreamViewImpl)!
-            view.startStreaming(requestId: Int(truncating: requestId), streamKey: streamKey, url: url)
-        }
+    uiManager.addUIBlock { uiManager, _ in
+      guard let view = uiManager?.view(forReactTag: reactTag) as? RNLiveStreamViewImpl else {
+        NSLog("[RNLiveStreamViewManager] view for tag \(reactTag) not found")
+        return
+      }
+      view.stopStreaming()
+    }
+  }
+
+  @objc(setZoomRatioCommand:withZoomRatio:)
+  func setZoomRatioCommand(_ reactTag: NSNumber, zoomRatio: NSNumber) {
+    guard let uiManager = bridge?.uiManager else {
+      NSLog("[RNLiveStreamViewManager] bridge/uiManager is nil")
+      return
     }
 
-    @objc(stopStreaming:)
-    func stopStreaming(_ reactTag: NSNumber) {
-        bridge!.uiManager.addUIBlock { (_: RCTUIManager?, viewRegistry: [NSNumber: UIView]?) in
-            let view: RNLiveStreamViewImpl = (viewRegistry![reactTag] as? RNLiveStreamViewImpl)!
-            view.stopStreaming()
-        }
+    uiManager.addUIBlock { uiManager, _ in
+      guard let view = uiManager?.view(forReactTag: reactTag) as? RNLiveStreamViewImpl else {
+        NSLog("[RNLiveStreamViewManager] view for tag \(reactTag) not found")
+        return
+      }
+      view.zoomRatio = zoomRatio.floatValue
     }
-
-    @objc(setZoomRatioCommand:withZoomRatio:)
-    func setZoomRatioCommand(_ reactTag: NSNumber, zoomRatio: NSNumber) {
-        bridge!.uiManager.addUIBlock { (_: RCTUIManager?, viewRegistry: [NSNumber: UIView]?) in
-            let view: RNLiveStreamViewImpl = (viewRegistry![reactTag] as? RNLiveStreamViewImpl)!
-            view.zoomRatio = zoomRatio.floatValue
-        }
-    }
+  }
 }
